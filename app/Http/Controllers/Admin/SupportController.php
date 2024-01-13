cb7d85b039ee3b4f09db02d94af3bd2390ffa8d7
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2024-01-13 18:37:04 -0300

    feat: Creating model and controller Support and implementing.

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
+    private $support;
+
+    public function __construct(Support $support)
+    {
+        $this->support = $support;
+    }
+
     public function index()
     {
-        return view('admin.supports.index');
+        $supports = $this->support->all();
+
+        return view('admin.supports.index', [
+            'supports' => $supports
+        ]);
     }
 }
