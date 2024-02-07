74d93f30f34f7bd98e4e566ea3f957de407fa18b
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2024-02-07 16:17:16 -0300

    feat: Application tailwind css.

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('admin.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('title', 'Suportes')
 
-<body>
+@section('header')
     <h1>Listagem dos Suportes</h1>
+@endsection
 
+@section('content')
     <a href="{{ route('supports.create') }}">Criar Dúvida</a>
 
     <table>
@@ -37,6 +33,5 @@
     </table>
 
     <x-pagination :paginator="$supports" :appends="$filter" />
-</body>
 
-</html>
+@endsection
