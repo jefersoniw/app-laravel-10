f68e74308f4245819f4a27fc880e47284c5297bc
Jeferson Chagas Silva <jeferson.chagas@salvador.ba.gov.br>
2024-03-01 15:10:37 -0300

    feat: package icons font-awesome. Create button register user and logout.

--- resources/views/admin/supports/partials/header.blade.php
+++ resources/views/admin/supports/partials/header.blade.php
@@ -20,6 +20,15 @@
 
             <span>Nova Dúvida</span>
         </a>
+        <form action="{{ route('logout') }}" method="post">
+            @csrf
+
+            <button type="submit"
+                class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
+                <span>Sair</span>
+            </button>
+        </form>
     </div>
 </div>
 
